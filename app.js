3e11b30f4296e0a7ab4207281d2c47d27de096e5
Rupert Brandstaetter <rupert.brandstaetter@gmail.com>
2020-08-27 23:04:43 +0200

    Calculate velocity with input

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 var app = new Vue({
   el:"#setup",
   data: {
+    flow_rate: '',
+    flow_rate_unit: '',
+    density_fluid: '',
+    density_fluid_unit: '',
+    density_atm: '',
+    density_atm_unit: '',
+    height_difference: '',
+    height_difference_unit: '',
     pipes: [
       {
         type: 'pipe',
-        length: '',
-        length_unit: 'Einheit',
+        pipe_length: '',
+        pipe_length_unit: 'Einheit',
         diameter: '',
         diameter_unit: 'Einheit',
         roughness: '',
-        roughness_unit: 'Einheit'
+        roughness_unit: 'Einheit',
       }
     ],
     fittings: [
@@ -24,8 +32,8 @@ var app = new Vue({
     addNewPipeForm () {
       this.pipes.push({
         type: 'pipe',
-        length: '',
-        length_unit: 'Einheit',
+        pipe_length: '',
+        pipe_length_unit: 'Einheit',
         diameter: '',
         diameter_unit: 'Einheit',
         roughness: '',
@@ -46,7 +54,71 @@ var app = new Vue({
       this.fittings.splice(index, 1)
     },
     printPipes () {
-      alert(this.pipes);
+      this.pipes.forEach( function (value) {
+        console.log(lengthConverter(value.length, value.length_unit))
+      });
+    },
+    calculateVelocity (pipe) {
+      let flow_rate = this.flow_rate;
+      let flow_rate_unit = this.flow_rate_unit;
+
+      flow_rate = flowRateConverter(flow_rate, flow_rate_unit);
+      let diameter = lengthConverter(pipe.diameter, pipe.diameter_unit);
+      let area = Math.pow(diameter, 2) * Math.PI / 4
+      let velocity = flow_rate / area
+
+      return velocity;
+    }
+  },
+  computed: {
+    velocity: function() {
+      let flow_rate = this.flow_rate;
+      let flow_rate_unit = this.flow_rate_unit;
+
+      flow_rate = flowRateConverter(flow_rate, flow_rate_unit);
+
+      this.pipes.forEach(function(pipe) {
+        pipe_length = lengthConverter(pipe.pipe_length, pipe.pipe_length_unit);
+        console.log(pipe_length);
+        console.log(pipe.pipe_length);
+        console.log(pipe.pipe_length_unit);
+      });
+
+      return flow_rate
     }
   }
 })
+
+/* When the input field receives input, convert the value */
+function lengthConverter(valNum, unit) {
+  if (unit == 'mm') {
+    valNum = valNum / 1000;
+  };
+  if (unit == 'm') {
+    valNum = valNum;
+  };
+  if (unit == 'cm') {
+    valNum = valNum / 100;
+  };
+  if (unit == 'inch') {
+    valNum = valNum / 39.37;
+  }
+  return valNum;
+}
+
+/* When the input field receives input, convert the value */
+function flowRateConverter(valNum, unit) {
+  if (unit == 'm3/s') {
+    valNum = valNum;
+  };
+  if (unit == 'm3/h') {
+    valNum = valNum / 60 / 60;
+  };
+  if (unit == 'L/min') {
+    valNum = valNum * (1/1000) / 60;
+  };
+  if (unit == 'L/s') {
+    valNum = valNum * (1/1000);
+  }
+  return valNum;
+}
